47a3171452ad30b9756a76b8fe50d7768f29e7d7
jonghoon1978 <86106738+jonghoon1978@users.noreply.github.com>
2021-12-07 19:41:19 +0900

    Redux: user.js

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,23 +2,28 @@ import React from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { actionCreators as userActions } from "../redux/modules/user";
+
 
 const Login = (props) => {
   const [id, setId] = React.useState("");
   const [pwd, setPwd] = React.useState("");
 
-  const changeId = (e) => {
-    setId(e.target.value);
-  };
+  // const changeId = (e) => {
+  //   setId(e.target.value);
+  // };
 
-  const changePwd = (e) => {
-    setPwd(e.target.value);
-  };
+  // const changePwd = (e) => {
+  //   setPwd(e.target.value);
+  // };
 
-  const login = () => {
-    // setCookie("user_id", id, 3);
-    // setCookie("user_pwd", pwd, 3);
-  };
+  // const login = () => {
+  //   setCookie("user_id", id, 3);
+  //   setCookie("user_pwd", pwd, 3);
+  // };
+
+  const dispatch = useDispatch()
   return (
     <React.Fragment>
       <Wrap>
@@ -28,21 +33,22 @@ const Login = (props) => {
           required
           id="outlined-required"
           label="E-mail"
-          onChange={(e) => console.log(e.target.value)}
+          onChange={(e) => setId(e.target.value)}
         />
         <TextField
           required
           type="password"
           id="outlined-required"
           label="PASSWORD"
-          onChange={(e) => console.log(e.target.value)}
+          onChange={(e) => setPwd(e.target.value)}
         />
         <Button
           variant="contained"
           disableElevation
           color="error"
           onClick={() => {
-            login();
+            // login();
+            dispatch(userActions.loginAction(id, pwd));
           }}>
           LOG IN
         </Button>
---
+++ src/redux/modules/user.js
@@ -0,0 +1,51 @@
+import { createAction, handleActions } from "redux-actions";
+import { produce } from "immer";
+
+
+const LOG_OUT = "LOG_OUT";
+const GET_USER = "GET_USER";
+const SET_USER = "SET_USER";
+
+ const logOut = createAction(LOG_OUT, (user) => ({ user }));
+const getUser = createAction(GET_USER, (user) => ({ user }));
+const setUser = createAction(SET_USER, (user) => ({ user }));
+
+ const initialState = {
+  user: null,
+  is_login: false,
+};
+
+ const loginAction = (user) => {
+  return function (dispatch, getState, { history }) {
+    console.log(history);
+    dispatch(setUser(user));
+    history.push("/");
+  };
+};
+
+ export default handleActions(
+  {
+    [SET_USER]: (state, action) =>
+      produce(state, (draft) => {
+        // setCookie("is_login", "success");
+        draft.user = action.payload.user;
+        draft.is_login = true;
+      }),
+    [LOG_OUT]: (state, action) =>
+      produce(state, (draft) => {
+        // deleteCookie("is_login");
+        draft.user = null;
+        draft.is_login = false;
+      }),
+    [GET_USER]: (state, action) => produce(state, (draft) => {}),
+  },
+  initialState
+);
+
+ const actionCreators = {
+  logOut,
+  getUser,
+  loginAction,
+ };
+
+export { actionCreators };
