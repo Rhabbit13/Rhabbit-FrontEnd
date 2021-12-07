17bbf9875af2f5832453da77d585b277eb3eeef4
dogmaster <cwd3469@naver.com>
2021-12-07 19:18:05 +0900

    Detail & Fix page set

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,17 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = props => {
   return (
-    <Box sx={{ flexGrow: 2, backgroundColor: "#eee" }}>
+    <Box
+      sx={{
+        flexGrow: 2,
+        backgroundColor: "#eee",
+        width: "100%",
+        marginBottom: "50px",
+      }}
+    >
       <AppBar position="static" style={{ background: "#eee", color: "#000" }}>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
--- src/pages/Fix.jsx
+++ src/pages/Fix.jsx
@@ -1,7 +1,100 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import styled from "styled-components";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Header from "../components/Header";
+import { useHistory } from "react-router-dom";
+import Edit from "../components/Edit";
 
-const Fix = () => {
-  return <div>Fix</div>;
+const FixPage = () => {
+  const percentage = 30;
+  const history = useHistory();
+  const mode = "disabled";
+  const Bar = styled.div`
+    background-color: #de4640;
+    width: ${percentage}%;
+    height: 100%;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+  `;
+  return (
+    <Grid
+      container
+      style={{
+        marginTop: "0px",
+        marginLeft: "0px",
+        width: "100%",
+      }}
+      rowSpacing={3}
+      columnSpacing={{ xs: 2, sm: 2, md: 3 }}
+    >
+      <Header></Header>
+      <Level>
+        <Percentage>{percentage}%</Percentage>
+        <Bar></Bar>
+      </Level>
+      {Array.from(Array(5)).map((_, index) => (
+        <Edit key={index} mode={mode}></Edit>
+      ))}
+      <Stack
+        direction="row"
+        spacing={2}
+        style={{
+          width: "100%",
+          position: "absolute",
+          left: "0px",
+          padding: "20px",
+          bottom: "10px",
+          boxSizing: "border-box",
+        }}
+      >
+        <Button
+          variant="contained"
+          style={{ width: "50%", backgroundColor: "#999", height: "40px" }}
+        >
+          수정하기
+        </Button>
+        <Button
+          variant="contained"
+          style={{
+            width: "50%",
+            backgroundColor: "#eee",
+            color: "#999",
+            height: "40px",
+          }}
+          onClick={() => {
+            history.goBack();
+          }}
+        >
+          뒤로 돌아가기
+        </Button>
+      </Stack>
+    </Grid>
+  );
 };
+const Level = styled.div`
+  width: 100%;
+  padding: 20px;
+  border-radius: 5px;
+  border: 1px solid #999;
+  margin-bottom: 30px;
+  position: relative;
+  overflow: hidden;
+`;
+const Percentage = styled.h3`
+  font-size: 23px;
+  color: #eee;
+  position: absolute;
+  left: 10px;
+  top: 10px;
+  z-index: 99;
+`;
 
-export default Fix;
+const Title = styled.h2`
+  font-size: 30px;
+  marg
+`;
+
+export default FixPage;
