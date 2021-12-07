03b4273828b5fb097eb7f089585f129f547003b0
jonghoon1978 <86106738+jonghoon1978@users.noreply.github.com>
2021-12-07 16:23:00 +0900

    로그인 사인업 페이지

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import styled from "styled-components";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = (props) => {
+  const [id, setId] = React.useState("");
+  const [pwd, setPwd] = React.useState("");
+
+  const changeId = (e) => {
+    setId(e.target.value);
+  };
+
+  const changePwd = (e) => {
+    setPwd(e.target.value);
+  };
+
+  const login = () => {
+    // setCookie("user_id", id, 3);
+    // setCookie("user_pwd", pwd, 3);
+  };
+  return (
+    <React.Fragment>
+      <Wrap>
+        <h1 type="heading">로그인 페이지</h1>
+
+        <TextField
+          required
+          id="outlined-required"
+          label="E-mail"
+          onChange={(e) => console.log(e.target.value)}
+        />
+        <TextField
+          required
+          type="password"
+          id="outlined-required"
+          label="PASSWORD"
+          onChange={(e) => console.log(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          disableElevation
+          color="error"
+          onClick={() => {
+            login();
+          }}>
+          LOG IN
+        </Button>
+      </Wrap>
+    </React.Fragment>
+  );
 };
+
+const Wrap = styled.div`
+gap: 40px;
+display: flex;
+flex-direction: column;
+justify-items:center;
+item-align: center;
+width: 80%;
+margin: auto;
+`;
+
 
 export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,105 @@
 import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import styled from "styled-components";
 
-const Signup = () => {
-  return <div>Signup</div>;
+const Signup = (props) => {
+  const [id, setId] = React.useState("");
+  const [pwd, setPwd] = React.useState("");
+  const [pwd_check, setPwdCheck] = React.useState("");
+  const [user_name, setUserName] = React.useState("");
+  const [emailCheck, setEmailCheck] = React.useState(true);
+
+  const isEmail = (id) => {
+    const emailRegex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+
+    return emailRegex.test(id);
+  };
+
+  const signup = () => {
+    console.log(isEmail(id));
+
+    if (isEmail(id) === false) {
+      setEmailCheck(false);
+      return;
+    }
+
+    if (id === "" || pwd === "" || user_name === "") {
+      return alert("E-mail 혹은 User Name을 입력하세요");
+    }
+
+    if (pwd !== pwd_check) {
+      return alert("비밀번호를 확인하세요.");
+    }
+
+    // dispatch(userActions.signupFB(id, pwd, user_name));
+  };
+  return (
+    <React.Fragment>
+      <Wrap>
+        <h1 type="heading">회원가입 페이지</h1>
+        <TextField
+          required
+          id="outlined-required"
+          label="이메일로 가입하기"
+          onChange={(e) => {
+            setId(e.target.value);
+            if (isEmail(id) === true) {
+              setEmailCheck(true);
+              return;
+            }
+          }}
+        />
+        {emailCheck === false ? <p>이메일 형식이 아닙니다. </p> : ""}
+        <TextField
+          required
+          id="outlined-required"
+          label="User Name"
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="비빌번호  "
+          onChange={(e) => {
+            setPwd(e.target.value);
+          }}
+        />
+        <TextField
+          required
+          type="password"
+          id="outlined-required"
+          label="비밀번호 확인"
+          onChange={(e) => {
+            setPwdCheck(e.target.value);
+          }}
+        />
+
+        <Button
+          variant="contained"
+          disableElevation
+          color="error"
+          onClick={() => {
+            // isEmail();
+            signup();
+          }}>
+          회원 가입하기
+        </Button>
+      </Wrap>
+    </React.Fragment>
+  );
 };
+const Wrap = styled.div`
+  gap: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  item-align: center;
+  width: 80%;
+  margin: auto;
+`;
 
 export default Signup;
